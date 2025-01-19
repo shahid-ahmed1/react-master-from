bcb28428e3463b22f7664bd731174689b052f4ad
shahid-ahmed1 <shahidcse2122gmail,com>
2025-01-19 22:31:55 +0600

    completed stateFullFrom in react router

--- src/components/StatefullFrom/StatefullFrom.jsx
+++ src/components/StatefullFrom/StatefullFrom.jsx
@@ -1,26 +1,48 @@
 import React, { useState } from 'react';
 
 const StatefullFrom = () => {
-    const [email,setEmail]=useState(null)
+    const [name ,setName]=useState(null);
+    const [email,setEmail]=useState(null);
+    const [password,setPassword]=useState(null);
+    const [error,setError]=useState('')
+    
     const handleSubmit =e=>{
         e.preventDefault()
-        console.log(email)
+         if(password.langth < 6){
+            setError('plese paswoed minimum 6 dizit')
+            console.log('errror')
+          }
+          else{
+            setError('')
+            console.log(name,email,password);
+          }
         
-
-
+        
+        
     }
+    const handleNameChange=e=>{
+        setName(e.target.value)
+   }
     const handleEmailChange = e =>{
      
      setEmail(e.target.value)
     }
+    const handlePasswordChange = e =>{
+     
+     setPassword(e.target.value)
+    }
+
     return (
         <div>
             <form  onSubmit={handleSubmit}>
                 <h4>Plese fillup the from</h4>
-                <input type="text" name='name' placeholder='Enter your name' /><br />
+                <input type="text" name='name' placeholder='Enter your name' onChange={handleNameChange} /><br />
                 <input type="email" name='email' onChange={handleEmailChange} placeholder='Enter your email'/><br />
-                <input type="password" name='password' placeholder='Enter your password'/><br />
+                <input type="password" name='password' placeholder='Enter your password' onChange={handlePasswordChange} required/><br />
                 <button>submit</button>
+                {
+                    error && <p>{error}</p>
+                }
             </form>
         </div>
     );
